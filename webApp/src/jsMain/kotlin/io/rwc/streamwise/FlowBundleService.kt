2aef43186ce361c0e3d4463a04638d373102f7b8
David Haley <dchaley@gmail.com>
2025-08-31 18:15:15 -0700

    Extract helper function to read bundle flows

--- webApp/src/jsMain/kotlin/io/rwc/streamwise/FlowBundleService.kt
+++ webApp/src/jsMain/kotlin/io/rwc/streamwise/FlowBundleService.kt
@@ -1,69 +1,35 @@
 package io.rwc.streamwise
 
-import io.rwc.streamwise.flows.Fixed
-import io.rwc.streamwise.flows.Monthly
-import io.rwc.streamwise.flows.accumulateFlows
+import io.rwc.streamwise.flows.*
 import kangular.core.Signal
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import kotlinx.datetime.LocalDate
+import kotlinx.serialization.KSerializer
 
 class FlowBundleService {
   private var monthlysCollector: Job? = null
 
   fun start(
     targetBalances: Signal<List<Fixed>>,
-    bundlesSignal: Signal<Array<io.rwc.streamwise.flows.FlowBundle>>,
+    bundlesSignal: Signal<Array<FlowBundle>>,
     startDate: LocalDate,
     endDate: LocalDate,
   ) {
     stop()
 
     val bundles = bundlesSignal()
-    val db = StreamFire.dataService.db
+    val subCollectionFlows = listOf(
+      bundles.readCashFlows("fixedFlows", Fixed.serializer()),
+      bundles.readCashFlows("monthlyFlows", Monthly.serializer()),
+    )
+    val allFlows = combine(subCollectionFlows) { flowLists -> flowLists.flatMap { it } }
 
-    // Build the combined flow of all bundles' monthlyFlows subcollections
-    val allMonthlys = combine(bundles.map { bundle ->
-      val doc = db.collection("flowBundles").document(bundle.id).collection("monthlyFlows")
-      doc.snapshots
-    }) { snapshotsArray ->
-      snapshotsArray.flatMap { snapshot ->
-        snapshot.documents.mapNotNull { doc ->
-          try {
-            doc.data(Monthly.serializer())
-          } catch (e: Exception) {
-            console.error("Error deserializing Monthly from doc ${doc.id}: $e. Cause: ${e.cause}")
-            null
-          }
-        }
-      }
-    }
-
-    val allFixed = combine(bundles.map { bundle ->
-      val doc = db.collection("flowBundles").document(bundle.id).collection("fixedFlows")
-      doc.snapshots
-    }) { snapshotsArray ->
-      snapshotsArray.flatMap { snapshot ->
-        snapshot.documents.mapNotNull { doc ->
-          try {
-            doc.data(Fixed.serializer())
-          } catch (e: Exception) {
-            console.error("Error deserializing Fixed from doc ${doc.id}: $e. Cause: ${e.cause}")
-            null
-          }
-        }
-      }
-    }
-
-    val allFlows = combine(allMonthlys, allFixed) { monthlys, fixeds ->
-      monthlys + fixeds
-    }
-
-    // Track the collector so it can be canceled/restarted as necessary
     monthlysCollector = CoroutineScope(Dispatchers.Main).launch {
       allFlows.collectLatest { flows ->
         targetBalances.value = accumulateFlows(flows, startDate, endDate)
@@ -74,5 +40,25 @@ class FlowBundleService {
   fun stop() {
     monthlysCollector?.cancel()
     monthlysCollector = null
+  }
+}
+
+fun <T : CashFlow> Array<FlowBundle>.readCashFlows(type: String, serializer: KSerializer<T>): Flow<List<CashFlow>> {
+  val db = StreamFire.dataService.db
+
+  return combine(this.map { bundle ->
+    val doc = db.collection("flowBundles").document(bundle.id).collection(type)
+    doc.snapshots
+  }) { snapshotsArray ->
+    snapshotsArray.flatMap { snapshot ->
+      snapshot.documents.mapNotNull { doc ->
+        try {
+          doc.data(serializer)
+        } catch (e: Exception) {
+          console.error("Error deserializing Fixed from doc ${doc.id}: $e. Cause: ${e.cause}")
+          null
+        }
+      }
+    }
   }
 }
