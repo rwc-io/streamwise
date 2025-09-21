4225df2ab06e389d66f6cbb15f849b5f1fad2519
David Haley <dchaley@gmail.com>
2025-09-21 01:25:08 -0700

    Fixup to description

--- shared/src/commonMain/kotlin/io/rwc/streamwise/flows/Descriptions.kt
+++ shared/src/commonMain/kotlin/io/rwc/streamwise/flows/Descriptions.kt
@@ -7,20 +7,24 @@ package io.rwc.streamwise.flows
  * can handle and display a generic error message.
  */
 fun CashFlow.describe(): String = when (this) {
-  is Fixed -> "Fixed on ${this.date}: ${this.amount}"
+  is Fixed -> describeFixed(this)
   is Monthly -> describeMonthly(this)
   is Weekly -> describeWeekly(this)
   is Yearly -> describeYearly(this)
 }
 
+fun describeFixed(flow: Fixed): String {
+  return "Fixed on ${flow.date}: ${flow.amount.toStringExpanded()}"
+}
+
 fun describeMonthly(flow: Monthly): String {
-  return "Monthly ${flow.name} on day offset ${flow.dayOffset}: ${flow.amount}"
+  return "Monthly ${flow.name} on day offset ${flow.dayOffset}: ${flow.amount.toStringExpanded()}"
 }
 
 fun describeWeekly(flow: Weekly): String {
-  return "Weekly ${flow.name} starting ${flow.startDate} (every ${flow.skip + 1} week${if (flow.skip == 0) "" else "s"}): ${flow.amount}"
+  return "Weekly ${flow.name} starting ${flow.startDate} (every ${flow.skip + 1} week${if (flow.skip == 0) "" else "s"}): ${flow.amount.toStringExpanded()}"
 }
 
 fun describeYearly(flow: Yearly): String {
-  return "Yearly ${flow.name} starting ${flow.startDate} (every ${flow.skip + 1} year${if (flow.skip == 0) "" else "s"}): ${flow.amount}"
+  return "Yearly ${flow.name} starting ${flow.startDate} (every ${flow.skip + 1} year${if (flow.skip == 0) "" else "s"}): ${flow.amount.toStringExpanded()}"
 }
